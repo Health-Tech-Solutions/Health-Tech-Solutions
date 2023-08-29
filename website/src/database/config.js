60aa7434a6de640a9ac977ddb546d68d885c361b
brunamatuciak <bruna.guedes@sptech.school>
2023-08-28 22:04:35 -0300

    update: nome_Usuario

--- website/src/database/config.js
+++ website/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "hts",
     user: "root",
-    password: "clubpenguim",
+    password: "sherlock15!",
 };
 
 function executar(instrucao) {

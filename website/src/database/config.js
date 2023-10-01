fb710bc1316de8161dc35344a62a07b4648b43db
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2023-10-01 16:37:53 -0300

    Começando a fazer o upload de imagem

--- website/src/database/config.js
+++ website/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "hts",
-    user: "stockSafe",
-    password: "urubu100",
+    user: "root",
+    password: "royale5290",
 };
 
 function executar(instrucao) {

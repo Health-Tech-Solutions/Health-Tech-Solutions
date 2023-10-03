246152f894e09b1f05cd24c8133862b3ce48626d
sofhiautaka <sofhia.utaka@sptech.school>
2023-10-02 21:14:12 -0300

    Verificações de alertas/ pop-ups de alertas colocados no login e cadastro

--- website/public/assets/js/register.js
+++ website/public/assets/js/register.js
@@ -4,34 +4,34 @@ var planoEscolhido;
 document.getElementById("primeiroCadastro").style.display = "block";
 
 // Adicionando mascara nos inputs
-InputTelefone.addEventListener('keypress', () =>{
+InputTelefone.addEventListener('keypress', () => {
     let inputLength = InputTelefone.value.length
 
-    if(inputLength === 0){
+    if (inputLength === 0) {
         InputTelefone.value += '(';
-    } else if(inputLength === 3){
+    } else if (inputLength === 3) {
         InputTelefone.value += ')'
-    } else if(inputLength === 9){
+    } else if (inputLength === 9) {
         InputTelefone.value += '-'
     }
 })
 
-InputCNPJ.addEventListener('keypress', () =>{
+InputCNPJ.addEventListener('keypress', () => {
     let inputLength = InputCNPJ.value.length
-  
-    if(inputLength === 2 || inputLength === 6){
+
+    if (inputLength === 2 || inputLength === 6) {
         InputCNPJ.value += '.'
-    } else if(inputLength === 10){
+    } else if (inputLength === 10) {
         InputCNPJ.value += '/'
-    } else if(inputLength === 15){
+    } else if (inputLength === 15) {
         InputCNPJ.value += '-'
     }
 })
 
-InputCEP.addEventListener('keypress', ()=> {
+InputCEP.addEventListener('keypress', () => {
     let inputLength = InputCEP.value.length
-    
-    if(inputLength === 5){
+
+    if (inputLength === 5) {
         InputCEP.value += '-'
     }
 })
@@ -49,31 +49,65 @@ function trocarTela() {
 }
 
 
+
+
 function verifEmail() {
     var erro = false;
+    var nomeVar = InputNome.value;
     var emailVar = InputEmail.value;
+    var senhaVar = InputSenha.value;
+    var confSenhaVar = InputConfirmacaoSenha.value;
 
-    
-    if (InputNome.value == '') {
-        alert("Favor inserir o nome do usuário");
-        erro = true;
+    var mensagensErro = []; // Array para armazenar mensagens de erro
+
+    function adicionarErro(inputElement) {
+        inputElement.style.borderColor = '#9a0801'; // Define a borda da input como vermelha
+        inputElement.classList.add('erro-input'); // Adiciona uma classe CSS para estilizar a input com vermelho
     }
-    if (emailVar == "") {
-        alert("Favor fornecer um email");
-        erro = true;
+
+    function removerErro(inputElement) {
+        inputElement.style.borderColor = ''; // Remove a cor vermelha da borda da input
+        inputElement.classList.remove('erro-input'); // Remove a classe CSS de erro
     }
-    if (InputConfirmacaoSenha.value != InputSenha.value) {
-        alert('As senhas não correspondem');
+
+    if (nomeVar == '') {
+        mensagensErro.push('Favor, inserir o nome do usuário.');
+        adicionarErro(InputNome);
         erro = true;
+    } else {
+        removerErro(InputNome);
+    }
+    if (emailVar == '') {
+        mensagensErro.push('Não esqueça de fornecer um email.');
+        adicionarErro(InputEmail);
+        erro = true;
+    } else {
+        removerErro(InputEmail);
+    }
+    if (confSenhaVar != senhaVar) {
+        mensagensErro.push('As senhas não correspondem.');
+        adicionarErro(InputConfirmacaoSenha);
+        erro = true;
+    } else {
+        removerErro(InputConfirmacaoSenha);
     }
     if (emailVar.indexOf("@") == -1) {
-        alert('Insira um email válido')
+        mensagensErro.push('Insira um email válido.');
+        adicionarErro(InputEmail);
         erro = true;
+    } else {
+        removerErro(InputEmail);
     }
-    if (InputSenha.value == '' || InputConfirmacaoSenha.value == '') {
-        alert('Favor fornecer uma senha')
+    if (senhaVar == '' || confSenhaVar == '') {
+        mensagensErro.push('Forneça uma senha.');
+        adicionarErro(InputSenha);
+        adicionarErro(InputConfirmacaoSenha);
         erro = true;
+    } else {
+        removerErro(InputSenha);
+        removerErro(InputConfirmacaoSenha);
     }
+
     if (erro == false) {
         // Enviando o valor da nova input
         fetch("/usuarios/verifEmail", {
@@ -110,27 +144,59 @@ function verifEmail() {
 
         return false;
     }
+
+    // Exibir todas as mensagens de erro de uma vez
+    if (mensagensErro.length > 0) {
+        Swal.fire({
+            position: 'top',
+            title: 'Atenção!',
+            html: mensagensErro.join('<br>'), // Juntar mensagens com quebras de linha
+            confirmButtonColor: '#9a0801'
+        });
+    }
 }
+
 
 function cadastrarEndereco() {
     // tratamento do input - Eliminando caracteres especiais
     var cepEspecial = InputCEP.value;
-    var cepVar = cepEspecial.replace(/[^0-9]/g,"");
+    var cepVar = cepEspecial.replace(/[^0-9]/g, "");
     var erro = false;
     var numeroVar = InputNumero.value;
     var complementoVar = InputComplemento.value;
 
+    var mensagensErro = [];
+
+    function adicionarErro(inputElement) {
+        inputElement.style.borderColor = '#9a0801'; 
+        inputElement.classList.add('erro-input'); 
+    }
+
+    function removerErro(inputElement) {
+        inputElement.style.borderColor = ''; 
+        inputElement.classList.remove('erro-input'); 
+    }
+
     if (cepVar == undefined) {
-        alert("Favor inserir o CEP");
+        mensagensErro.push('Favor, inserir o CEP.');
+        adicionarErro(InputCEP);
         erro = true;
+    } else {
+        removerErro(InputCEP);
     }
     if (numeroVar == undefined) {
-        alert("Favor fornecer um número");
+        mensagensErro.push('Não esqueça de inserir o número.');
+        adicionarErro(InputNumero);
         erro = true;
+    } else {
+        removerErro(InputNumero);
     }
     if (complementoVar == undefined) {
-        alert('Insira um complemento');
+        mensagensErro.push('Insira um complemento.');
+        adicionarErro(InputComplemento);
         erro = true;
+    } else {
+        removerErro(InputComplemento);
     }
     if (erro == false) {
         fetch("/usuarios/cadastrarEndereco", {
@@ -163,12 +229,21 @@ function cadastrarEndereco() {
         return false;
     }
 
+    if (mensagensErro.length > 0) {
+        Swal.fire({
+            position: 'top',
+            title: 'Atenção!',
+            html: mensagensErro.join('<br>'), 
+            confirmButtonColor: '#9a0801'
+        });
+    }
+
 }
 
 function buscaFkEndereco() {
     var erro = false;
     var cepEspecial = InputCEP.value;
-    var cepVar = cepEspecial.replace(/[^0-9]/g,"");
+    var cepVar = cepEspecial.replace(/[^0-9]/g, "");
     var numeroVar = InputNumero.value;
     var complementoVar = InputComplemento.value;
 
@@ -203,7 +278,7 @@ function buscaFkEndereco() {
                     resposta.json().then(json => {
                         console.log(JSON.stringify(json));
                         if (json[0] != undefined) {
-                            fkEndereco= json[0].idEndereco;
+                            fkEndereco = json[0].idEndereco;
                             cadastrarEmpresa();
                         }
                         else {
@@ -221,15 +296,32 @@ function buscaFkEndereco() {
 
         return false;
     }
+
 }
 
 function verifCNPJ() {
     var erro = false;
     var cnpjVar = InputCNPJ.value;
     console.log(InputCNPJ)
+
+    var mensagensErro = [];
+
+    function adicionarErro(inputElement) {
+        inputElement.style.borderColor = '#9a0801'; 
+        inputElement.classList.add('erro-input'); 
+    }
+
+    function removerErro(inputElement) {
+        inputElement.style.borderColor = ''; 
+        inputElement.classList.remove('erro-input'); 
+    }
+
     if (cnpjVar == undefined || cnpjVar == '') {
-        alert('O CNPJ esta undefined');
+        mensagensErro.push('CNPJ inválido');
+        adicionarErro(InputCNPJ);
         erro = true;
+    } else {
+        removerErro(InputCNPJ);
     }
     if (erro == false) {
         fetch("/usuarios/verifCNPJ", {
@@ -267,6 +359,15 @@ function verifCNPJ() {
 
         return false;
     }
+
+    if (mensagensErro.length > 0) {
+        Swal.fire({
+            position: 'top',
+            title: 'Atenção!',
+            html: mensagensErro.join('<br>'), 
+            confirmButtonColor: '#9a0801'
+        });
+    }
 }
 
 function cadastrarEmpresa() {
@@ -279,17 +380,38 @@ function cadastrarEmpresa() {
     var nomeFantasiaVar = InputNomeFantasia.value;
     var fkEnderecoVar = fkEndereco;
 
+    var mensagensErro = [];
+
+    function adicionarErro(inputElement) {
+        inputElement.style.borderColor = '#9a0801'; 
+        inputElement.classList.add('erro-input'); 
+    }
+
+    function removerErro(inputElement) {
+        inputElement.style.borderColor = ''; 
+        inputElement.classList.remove('erro-input'); 
+    }
+
     if (nomeFantasiaVar == undefined) {
-        alert("O nome fantasia esta undefined");
+        mensagensErro.push('Nome fantasia indefinido');
+        adicionarErro(InputNomeFantasia);
         erro = true;
+    } else {
+        removerErro(InputNomeFantasia);
     }
     if (telefoneVar == undefined) {
-        alert("O número esta undefined");
+        mensagensErro.push('Colocar um número válido');
+        adicionarErro(InputTelefone);
         erro = true;
+    } else {
+        removerErro(InputTelefone);
     }
     if (cnpjVar == undefined) {
-        alert('O complemento esta undefined');
+        mensagensErro.push('CNPJ inválido');
+        adicionarErro(InputCNPJ);
         erro = true;
+    } else {
+        removerErro(InputCNPJ);
     }
     if (fkEnderecoVar == undefined) {
         alert('O complemento esta undefined');
@@ -324,6 +446,14 @@ function cadastrarEmpresa() {
             });
 
         return false;
+    }
+    if (mensagensErro.length > 0) {
+        Swal.fire({
+            position: 'top',
+            title: 'Atenção!',
+            html: mensagensErro.join('<br>'), 
+            confirmButtonColor: '#9a0801'
+        });
     }
 }
 
@@ -372,13 +502,14 @@ function buscarFkEmpresa() {
                     resposta.json().then(json => {
                         console.log(JSON.stringify(json));
                         if (json[0] != undefined) {
-                            fkEmpresa= json[0].idEmpresa;
+                            fkEmpresa = json[0].idEmpresa;
                             cadastrarFuncionario();
                         }
                         else {
                             console.log("Não foi possível encontrar a fk de empresa")
                         }
-                    })} else {
+                    })
+                } else {
                     throw "Não foi possível localizar fk de Empresa";
                 }
             })
@@ -395,20 +526,41 @@ function cadastrarFuncionario() {
     var erro = false;
     var nomeVar = InputNome.value;
     var emailVar = InputEmail.value;
-    var senhaVar = InputSenha.value;    
+    var senhaVar = InputSenha.value;
     var fkEmpresaVar = fkEmpresa;
 
+    var mensagensErro = [];
+
+    function adicionarErro(inputElement) {
+        inputElement.style.borderColor = '#9a0801'; 
+        inputElement.classList.add('erro-input'); 
+    }
+
+    function removerErro(inputElement) {
+        inputElement.style.borderColor = ''; 
+        inputElement.classList.remove('erro-input'); 
+    }
+
     if (nomeVar == undefined) {
-        alert("O nome esta undefined");
+        mensagensErro.push('Coloque um nome válido.');
+        adicionarErro(InputNome);
         erro = true;
+    } else {
+        removerErro(InputNome);
     }
     if (emailVar == undefined) {
-        alert("O email esta undefined");
+        mensagensErro.push('Insira um email válido.');
+        adicionarErro(InputEmail);
         erro = true;
+    } else {
+        removerErro(InputEmail);
     }
     if (senhaVar == undefined) {
-        alert('A senha esta undefined');
+        mensagensErro.push('Insira a sua senha.');
+        adicionarErro(InputSenha);
         erro = true;
+    } else {
+        removerErro(InputSenha);
     }
     if (fkEmpresaVar == undefined) {
         alert('O fk esta undefined');
@@ -447,6 +599,14 @@ function cadastrarFuncionario() {
             });
 
         return false;
+    }
+    if (mensagensErro.length > 0) {
+        Swal.fire({
+            position: 'top',
+            title: 'Atenção!',
+            html: mensagensErro.join('<br>'), 
+            confirmButtonColor: '#9a0801'
+        });
     }
 }
 

8e247e4a94b0cf6cbbbc0b87d8189daf0c72188e
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2023-10-10 20:33:23 -0300

    foto na página inicial

--- website/src/database/config.js
+++ website/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "hts",
-    user: "hts",
-    password: "urubu100",
+    user: "",
+    password: "",
 };
 
 function executar(instrucao) {

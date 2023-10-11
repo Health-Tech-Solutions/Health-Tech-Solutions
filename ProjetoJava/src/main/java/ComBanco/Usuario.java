dfe5e82da836afc6ab15473fe44580961e3f0a5c
sofhiautaka <sofhia.utaka@sptech.school>
2023-10-11 01:33:48 -0300

    Início da customização do Java

--- ProjetoJava/src/main/java/ComBanco/Usuario.java
+++ ProjetoJava/src/main/java/ComBanco/Usuario.java
@@ -12,20 +12,45 @@ public class Usuario {
     private String senha;
     private String empresa;
     private String cargo;
+
     private Scanner scanInt = new Scanner(System.in);
     private Scanner scanString = new Scanner(System.in);
+
+    private String enfeite = "-".repeat(65);
+    private String enfeite15 = "-".repeat(15);
+    private String igual30 = "=".repeat(30);
+
+    // cores
+
+    private String padrao = "\u001B[0m"; // redefinir a formatação depois de estilizar
+    private String negrito = "\u001B[1m";
+    private String vermelho = "\u001B[31m";
+    private String verde = "\u001B[32m";
+    private String amarelo = "\u001B[33m";
+    private String azul = "\u001B[34m";
+    private String magenta = "\u001B[35m";
+    private String ciano = "\u001B[36m";
+
 
     ConexaoBD conexao = new ConexaoBD();
     JdbcTemplate con = conexao.getConexaoBanco();
 
     public void menu() {
-        System.out.println("Bem-vindo ao sistema de monitoramento Health Tech Solutions!");
+        System.out.println(negrito + enfeite);
+        System.out.println(" Bem-vindo ao sistema de monitoramento Health Tech Solutions! ");
+        System.out.println(enfeite + negrito);
+
         while (true) {
-            System.out.println("""
-                    Selecione uma opção:
-                    1. Cadastro
-                    2. Login
-                    3. Sair""");
+            System.out.printf("""
+                    +---------------------------+
+                    | %s  Health Tech Solutions  %s |
+                    +---------------------------+
+                    |%s Selecione uma opção:      |
+                    | 1. Cadastro               |
+                    | 2. Login                  |
+                    | 3. Sair   %s                |
+                    +---------------------------+
+                    """, vermelho, padrao, negrito, padrao);
             Integer opcaoEscolhida = scanInt.nextInt();
 
             switch (opcaoEscolhida) {
@@ -44,6 +69,7 @@ public class Usuario {
     }
 
     public void cadastrar() {
+        System.out.println("\n" + negrito + ciano + enfeite15 + " CADASTRAR " + enfeite15 + padrao);
         System.out.println("Digite o nome do usuário:");
         String nomeCadastro = scanString.nextLine();
 
@@ -59,7 +85,7 @@ public class Usuario {
         }
 
         if (nomeJaExiste) {
-            System.out.println("Nome de usuário já existe! Escolha outro nome.");
+            System.out.println(vermelho + "Nome de usuário já existe! Escolha outro nome." + padrao);
             cadastrar();
         } else {
             System.out.println("Digite a senha:");
@@ -72,17 +98,20 @@ public class Usuario {
             con.update(
                     "INSERT INTO usuario (nome, senha, empresa, cargo) VALUES (?, ?, ?, ?);",
                     nomeCadastro, senhaCadastro, empresaCadastro, cargoCadastro);
-            System.out.println("""
-                    Usuário cadastrado com sucesso!
-                    Você está sendo redirecionado para o Login...""");
+            System.out.printf("""
+                    
+                            %sUsuário cadastrado com sucesso! %s
+                            Você está sendo redirecionado para o Login...
+                            """, verde, padrao);
             logar();
         }
     }
 
     public void logar() {
-        System.out.println("Digite o nome de usuário:");
+        System.out.println("\n" + negrito + ciano + enfeite15 + " LOGIN " + enfeite15 + padrao);
+        System.out.println("User:");
         String nomeLogin = scanString.nextLine();
-        System.out.println("Digite a senha:");
+        System.out.println("Senha:");
         String senhaLogin = scanString.nextLine();
 
         List<Usuario> login = con.query(
@@ -99,15 +128,15 @@ public class Usuario {
         }
 
         if (!loginValidado) {
-            System.out.println(""" 
-            Nome de usuário ou senha incorretos!
-            Ou talvez você não possua um cadastro
-            """);
+            System.out.printf(""" 
+                              \n%sNome de usuário ou senha incorretos!
+                              Ou talvez você não possua um cadastro%s
+                              """, vermelho, padrao);
 
             while (true) {
                 System.out.println("""
-                        1- Continuar no Login
-                        2- Fazer cadastro
+                      \n1- Continuar no Login
+                      2- Fazer cadastro
                         """);
                 Integer escolha = scanInt.nextInt();
 
@@ -117,7 +146,7 @@ public class Usuario {
                     cadastrar();
                     return;
                 } else {
-                    System.out.println("Opção inválida!");
+                    System.out.println(vermelho + "Opção inválida!" + padrao);
                 }
             }
         }
@@ -129,20 +158,21 @@ public class Usuario {
     }
 
     public void listarUsuarios() {
-        System.out.println("Lista de usuários:");
+        System.out.println("\n" + negrito + magenta + enfeite15 + " LISTA DE USUÁRIOS " + enfeite15 + padrao);
+        System.out.println(negrito + igual30 + padrao);
 
         List<Usuario> usuarios = con.query("SELECT * FROM usuario;",
                 new BeanPropertyRowMapper<>(Usuario.class));
         for (int i = 0; i < usuarios.size(); i++) {
-            System.out.println("=".repeat(30));
             Usuario usuarioDaLista = usuarios.get(i);
-            System.out.printf((i+1) + """
+            System.out.printf("""
                             ID do usuário: %d
                             Nome: %s
                             Empresa: %s
                             Cargo: %s
                             """, usuarioDaLista.getIdUsuario(), usuarioDaLista.getNome(),
                     usuarioDaLista.getEmpresa(), usuarioDaLista.getCargo());
+            System.out.println(negrito + igual30 + padrao);
             if (usuarios.size() == (i+1)) {
                 break;
             }

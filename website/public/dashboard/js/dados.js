4b8a5bd6694eb839b7cbfdb4e92ae31cdd841851
HenriqueBechis <Henrique.coelho@sptech.school>
2023-10-21 21:17:41 -0300

    puxando nome e foto em todas as telas da dash index

--- website/public/dashboard/js/dados.js
+++ website/public/dashboard/js/dados.js
@@ -8,53 +8,19 @@ function mostrarDados(){
       resposta.json()
       .then(
         function(resposta){
-          infos = resposta[0] 
+          infos = resposta[0]
           console.log("the resposta é " + infos)
-          // Foto_perfil = document.getElementById("foto_perfil")
-          // Perfil = document.getElementById("foto_perfil")
-          //   if(infos.foto == null){
-          //     Foto.src = `../assets/usuario.png`
-          //     Foto_perfil.src = `../assets/usuario.png                  `
-          //   }else{
-          //     Foto.src = `../assets/${infos.foto}`
-
-          //   }
-
+          let foto = document.getElementById("usuario_foto")
+          let nome = document.getElementById("usuario_nome")
+          nome.innerHTML = infos.nome
+          if(infos.foto == null){
+            foto.src = `../assets/usuario.png`
+          } else {
+            foto.src = `../assets/${infos.foto}`
+          }
         }
       )
     }
   })
 }
 
-
-function mostrarFoto(){
-    var idFuncionario = sessionStorage.ID_USUARIO;
-    fetch(`/funcionario/mostrarFoto/${idFuncionario}`)
-    .then(function (resposta){
-      if(resposta.ok){
-        resposta.json()
-        .then(
-
-          function(resposta){
-            infos = resposta[0];
-            console.log(infos)
-            Foto = document.getElementById("usuario_foto")
-            Foto_perfil = document.getElementById("foto_perfil")
-            Perfil = document.getElementById("foto_perfil")
-            if(infos.foto == null){
-              Foto.src = `../assets/usuario.png`
-              Foto_perfil.src = `../assets/usuario.png                  `
-            }else{
-              Foto.src = `../assets/${infos.foto}`
-
-            }
-            Perfil.src = `../assets/${infos.foto}`;
-
-          }
-        )
-      }
-    })
-    .catch(err => {
-      console.log("ERRO" + err)
-    })
-  }

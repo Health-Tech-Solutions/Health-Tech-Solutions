f4444ab70964df1a4e96ccce6ae6921bece63b20
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2023-11-05 15:36:27 -0300

    Gráfico linha dashGabriel funcionando

--- website/public/dashboard/js/chartLineGabriel.js
+++ website/public/dashboard/js/chartLineGabriel.js
@@ -23,7 +23,9 @@ var desempenho = []
                                 desempenho.push(resposta[index].valor)
                             }
 
-                            lineChart.update()
+                            criarGraficoLinha()
+
+                            //lineChart.update()
                      }
                  )
              }
@@ -39,50 +41,112 @@ var desempenho = []
 
 
 
-function plotarGrafico(resposta){
 
-    labels = dataDados
-    dados.datasets[0].data = []
+ function calcularRegressaoLinear() {
+    dataDados = dataDados.map(function (valor) {
+        return parseFloat(valor); // Converte a string em número
+    });
+
+    desempenho = desempenho.map(function (valor1) {
+        return parseFloat(valor1); // Converte a string em número
+    });
     
-    for (let i = 0; i < resposta.length; i++) {
-        let registro = resposta[i];
-        // console.log(registro.mes)
-        dados.datasets[0].data[registro.mes - 1] = (registro.quantidade)
+    // Calcular a média de x e y
+    var xSum = 0;
+    var ySum = 0;
+    for (var i = 0; i < dataDados.length; i++) {
+        xSum += dataDados[i];
+        ySum += desempenho[i];
     }
-    dados.labels = labels
-    lineChart.update()
+    console.log(xSum,ySum)
+    var xMean = xSum / dataDados.length;
+    var yMean = ySum / dataDados.length;
+    console.log(xMean,ySum)
+    
+    // Calcular os coeficientes da regressão
+    var numerator = 0;
+    var denominator = 0;
+    for (var i = 0; i < dataDados.length; i++) {
+        numerator += (dataDados[i] - xMean) * (desempenho[i] - yMean);
+        denominator += (dataDados[i] - xMean) * (dataDados[i] - xMean);
+        console.log(numerator,denominator)
+    }
+    var a = numerator / denominator;
+    var b = yMean - a * xMean;
+
+    console.log(a, b);
+    return { a, b };
 }
 
-data = [0,0,0,0,0,0,0,0,0,0,0,0]
-const ctx = document.getElementById('chartLinha');
-labels = dataDados
-// 12, 19, 3, 5, 2, 3, 4, 7, 1, 2, 4, 7
-var dados = {
-    labels: dataDados,
-    datasets: [{
-        label: '',
-        data: desempenho,
-        borderWidth: 1,
-        backgroundColor: '#030050',
-        borderColor: '#030050'
-    }]
-}
+ // Função para criar o gráfico de linha
+function criarGraficoLinha() {
+    // Calcula os coeficientes da regressão linear
+    var coeficientes = calcularRegressaoLinear();
+    var a = coeficientes.a;
+    var b = coeficientes.b;
 
-var lineChart = new Chart(ctx, {
-    type: 'line',
-    data: dados,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+    // Preparar os dados para o gráfico
+    var labels = dataDados;
+    var valoresRegressao = dataDados.map(function (x) {
+        return a * x + b;
+    });
+
+    // Configuração do gráfico
+    var ctx = document.getElementById('chartLinha').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Desempenho Original',
+                    data: desempenho,
+                    borderColor: 'blue',
+                    fill: false,
+                },
+                {
+                    label: 'Regressão Linear',
+                    data: valoresRegressao,
+                    borderColor: 'red',
+                    fill: false,
+                },
+            ],
         },
-        plugins: {
-            legend: {
-                display: false
-            }
-        }
-    }
-});
+    });
+}
 
 
+
+
+//GRÁFICO ANTIGO
+
+// const ctx = document.getElementById('chartLinha');
+// labels = dataDados
+// // 12, 19, 3, 5, 2, 3, 4, 7, 1, 2, 4, 7
+// var dados = {
+//     labels: dataDados,
+//     datasets: [{
+//         label: '',
+//         data: desempenho,
+//         borderWidth: 1,
+//         backgroundColor: '#030050',
+//         borderColor: '#030050'
+//     }]
+// }
+
+// var lineChart = new Chart(ctx, {
+//     type: 'line',
+//     data: dados,
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         },
+//         plugins: {
+//             legend: {
+//                 display: false
+//             }
+//         }
+//     }
+// });

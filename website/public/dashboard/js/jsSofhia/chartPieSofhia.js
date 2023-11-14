33c29f8f8503de825573d4f0f17e42df968ae6b7
sofhiautaka <sofhia.utaka@sptech.school>
2023-11-14 13:06:06 -0300

    Melhorando o select da model

--- website/public/dashboard/js/jsSofhia/chartPieSofhia.js
+++ website/public/dashboard/js/jsSofhia/chartPieSofhia.js
@@ -128,19 +128,19 @@ function buscarAlertasDosComponentesDoAno() {
 }
 
 function plotarGraficoPizza(resposta){
-    let labels = []
-    dadosPizza.labels = labels
+    let labelsPie = []
+    
     dadosPizza.datasets[0].data = []
 
     for (let index = 0; index < resposta.length; index++) {
-        let cpu = resposta[index].Total_de_Chamados_CPU;
-        let ram = resposta[index].Total_de_Chamados_RAM;
-        let disco = resposta[index].Total_de_Chamados_Disco;
+        let qtdComponente = resposta[index].quantidade;
+        let nomePeca = resposta[index].nomePeca;
 
-        dadosPizza.datasets[0].data.push(cpu)
-        dadosPizza.datasets[0].data.push(ram)
-        dadosPizza.datasets[0].data.push(disco)
+        labelsPie.push(nomePeca)
+        dadosPizza.datasets[0].data.push(qtdComponente)
+       
     }
+    dadosPizza.labels = labelsPie
     graficoPizza.update()
 }
 

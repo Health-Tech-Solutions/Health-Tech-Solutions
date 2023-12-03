9b510d9d0fdba3a2cf4538486e6980aceaa9fca8
HenriqueBechis <Henrique.coelho@sptech.school>
2023-12-03 04:07:58 -0300

    Dados organizados no gráfico

--- website/public/dashboard/js/jsHenrique/chartLineHenrique.js
+++ website/public/dashboard/js/jsHenrique/chartLineHenrique.js
@@ -1,58 +1,58 @@
 
-var fkHospital = sessionStorage.FK_HOSPITAL     
-function obterDadosMensais(){
- 
+var fkHospital = sessionStorage.FK_HOSPITAL
+function obterDadosMensais() {
+
     fetch(`/henrique/buscarMensal/${fkHospital}`)
-    .then(
-        function(resposta){
-            if(resposta.ok){
-                resposta.json()
-                .then(
-                    function(resposta){
-                        console.log(resposta) 
-                        plotarGrafico(resposta)
-                    }
-                )
+        .then(
+            function (resposta) {
+                if (resposta.ok) {
+                    resposta.json()
+                        .then(
+                            function (resposta) {
+                                console.log(resposta)
+                                plotarGrafico(resposta)
+                            }
+                        )
+                }
             }
-        }
-    )
-    .catch(
-        err => {
-            console.log("ERRO " + err)
-        }
-    )
+        )
+        .catch(
+            err => {
+                console.log("ERRO " + err)
+            }
+        )
 }
 
-function obterDadosSemanal(){
+function obterDadosSemanal() {
     var fkHospital = sessionStorage.FK_HOSPITAL;
     fetch(`/henrique/buscarSemanal/${fkHospital}`)
-    .then(
-        function(resposta){
-            if(resposta.ok){
-                resposta.json()
-                .then(
-                    function(resposta){
-                        console.log(resposta)
-                        plotarGraficoSemanal(resposta)
-                    }
-                )
+        .then(
+            function (resposta) {
+                if (resposta.ok) {
+                    resposta.json()
+                        .then(
+                            function (resposta) {
+                                console.log(resposta)
+                                plotarGraficoSemanal(resposta)
+                            }
+                        )
+                }
             }
-        }
-    )
-    .catch(
-        err => {
-            console.log("ERRO " + err)
-        }
-    )
+        )
+        .catch(
+            err => {
+                console.log("ERRO " + err)
+            }
+        )
 }
 
-function plotarGraficoSemanal(resposta){
-    
+function plotarGraficoSemanal(resposta) {
+
     labels = []
     data = []
     var dataAtual = new Date()
     var mes = dataAtual.getMonth() + 1
-    
+
     for (let i = 0; i < resposta.length; i++) {
 
         let ocorrencia = resposta[i];
@@ -60,19 +60,19 @@ function plotarGraficoSemanal(resposta){
         labels.push(ocorrencia.dia + '/' + mes)
         data.push(ocorrencia.quantidade)
     }
+    mediaSemanal = calcularMedia(data)
 
-    let mediaSemanal = data.reduce((acc,val) => acc = val,0) / data.length  
     media.innerHTML = 'Média de chamados por dia: ' + mediaSemanal.toFixed(0)
-    
+
     dados.labels = labels
     dados.datasets[0].data = data
-  
+
     lineChart.update()
 }
 
 var dataRegressao = []
-function plotarGrafico(resposta){
-    
+function plotarGrafico(resposta) {
+
     labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
     dados.datasets[0].data = []
     var dadosMedia = []
@@ -80,16 +80,16 @@ function plotarGrafico(resposta){
         let registro = resposta[i];
         dados.datasets[0].data[registro.mes - 1] = (registro.quantidade)
         dadosMedia.push(registro.quantidade)
-    }   
-    
-    var mediaDados = dadosMedia.reduce((acc, val) => acc = val,0) /dadosMedia.length
+    }
+
+    var mediaDados = dadosMedia.reduce((acc, val) => acc = val, 0) / dadosMedia.length
     media.innerHTML = 'Média de chamados por mês: ' + mediaDados.toFixed(0)
 
     dados.labels = labels
     lineChart.update()
 }
 
-data = [0,0,0,0,0,0,0,0,0,0,0,0]
+data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 const ctx = document.getElementById('chartLinha');
 labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 var dados = {
@@ -126,82 +126,71 @@ var lineChart = new Chart(ctx, {
     }
 });
 
-function predicao(){
+function predicao() {
     var dadosReais = dados.datasets[0].data
     var vetorAux = []
     var coeficientes = calcularCoeficientes(dadosReais)
     var angular = coeficientes[0].toFixed(0)
     var linear = coeficientes[1].toFixed(0)
-    for(let i = 0; i < dadosReais.length; i++){
-        // let formula = linearidade(dadosReais[i],linear,dadosReais) + angular * i
-        // let formulaDif = diferenca(dadosReais[i],linear) + angular * i                   
-        // let formulaDif = dadosReais[i]
+    for (let i = 0; i < dadosReais.length; i++) {
 
         let formula = Number(linear) + Number(angular) * i
-
-
-        // alert(dadosReais[i])
-        // alert(formula)
-
-        if(dadosReais[i] < formula){
-            if(dadosReais[i] < (formula * 0.8)){
+        
+        if (dadosReais[i] < formula) {
+            if (dadosReais[i] < (formula * 0.8)) {
                 dadosReais[i] = formula * 0.8
             }
-        } else if(dadosReais[i] > formula){
-            if(dadosReais[i] > (formula * 1.1)){
+        } else if (dadosReais[i] > formula) {
+            if (dadosReais[i] > (formula * 1.1)) {
                 dadosReais[i] = formula * 1.1
             }
         }
- 
-        // if(formulaDif > formula * 1.15 || formulaDif < formula){
-        //     formulaDif = formula * 1.15 
-        // }
-     
-        // if()
+        
         vetorAux.push(dadosReais[i])
     }
-    var mediaAux = vetorAux.reduce((acc,val) => acc + val, 0)/vetorAux.length
+    
+    var mediaAux = vetorAux.reduce((acc, val) => acc + val, 0) / vetorAux.length
     media.innerHTML = `Média prevista: ${mediaAux.toFixed(0)}`
     dados.datasets[0].data = vetorAux
     lineChart.update()
 }
 
-function calcularCoeficientes(dataset){
+function calcularCoeficientes(dataset) {
     const mediaY = dataset.reduce((acc, val) => acc + val, 0) / dataset.length;
 
-   const valoresX = Array.from({ length: dataset.length }, (_, index) => index + 1);
-   
+    const valoresX = Array.from({ length: dataset.length }, (_, index) => index + 1);
+
     const mediaX = calcularMedia(valoresX);
 
-   let numeradorM = 0;
-   let denominadorM = 0;
-   
-   for (let i = 0; i < valoresX.length; i++) {
-     numeradorM += (valoresX[i] - mediaX) * (dataset[i] - mediaY);
-     denominadorM += Math.pow(valoresX[i] - mediaX, 2);
-   }
+    let numeradorM = 0;
+    let denominadorM = 0;
 
-   const angular = numeradorM / denominadorM;
-   
+    for (let i = 0; i < valoresX.length; i++) {
+        numeradorM += (valoresX[i] - mediaX) * (dataset[i] - mediaY);
+        denominadorM += Math.pow(valoresX[i] - mediaX, 2);
+    }
 
-   const linear = mediaY - angular * mediaX;
-   return [angular,linear]
+    const angular = numeradorM / denominadorM;
+
+
+    const linear = mediaY - angular * mediaX;
+    return [angular, linear]
 }
 
 function calcularMedia(dados) {
     return dados.reduce((acc, val) => acc + val, 0) / dados.length;
 }
 
-function linearidade(dado, coef, array){
+function linearidade(dado, coef, array) {
     let mediaArray = calcularMedia(array)
     let outLiers = 0;
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
-        if(element ){}
-        
+        if (element) { }
+
     }
 
-    return (num1 + (num1 - num2)) 
+    return (num1 + (num1 - num2))
 }
 
 function diferenca(num1, num2) {
